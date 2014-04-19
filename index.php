54806262232ae8116f66ecf5cbf18ef0f6a3f3f8
Benjamin Gröhbiel <benjamin.groehbiel@gmail.com>
2014-04-19 12:40:58 +0200

    ironed out tiny bugs

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
 
 require_once('phprestsql.php');
+require_once('urlparser.php');
 
 $PHPRestSQL = new PHPRestSQL();
 $PHPRestSQL->exec();
